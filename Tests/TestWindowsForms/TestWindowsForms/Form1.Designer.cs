aeab3e81bc8000d55188c1791a1125c752ccc806
U-jacob-gamey\Jacob Aimino <Jacob Aimino@jacob-lappytop.(none)>
2014-08-28 08:39:49 -0400

    Prettier now

--- Tests/TestWindowsForms/TestWindowsForms/Form1.Designer.cs
+++ Tests/TestWindowsForms/TestWindowsForms/Form1.Designer.cs
@@ -120,6 +120,7 @@
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(741, 456);
             this.textBox1.TabIndex = 0;
             // 
