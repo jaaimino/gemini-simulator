cc6718e1a50464c409b03a695bc5a8a80cf35648
Jacob A <Jacobaimino@gmail.com>
2014-09-10 16:30:12 -0400

    Additional test

--- Project1/Test.s
+++ Project1/Test.s
@@ -2,7 +2,7 @@
 main:
 	add #$10   tatjakj     ! Add 10 to the accumulator
 	and $10        ! And memory address 10 with the accumulator
-	lda $0  Hey- I forgot to put !
+ahtjkahtk	lda $0  Hey- I forgot to put !
 	nop
 	lda      #$10
 nope:
