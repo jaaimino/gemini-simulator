b16b81af360d14ae53e92bb05c757c926d34dab8
Jacob A <Jacobaimino@gmail.com>
2014-09-10 17:41:59 -0400

    Working on instruction encoding

---
+++ Project1/Project1/Utilities/Opcode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Opcode
+    {
+        Dictionary<String, String> opcodeToBinaryMap;
+        Dictionary<String, String> binaryToOpcodeMap;
+
+        public static void readConfig()
+        {
+
+        }
+
+        public static String opcodeToBinary()
+        {
+            return "";
+        }
+
+        public static String binaryToOpcode()
+        {
+            return "";
+        }
+    }
+}
